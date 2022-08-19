90738684c608461940dc2944caf1e797e6d68c71
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-08-19 08:22:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,27 +18,3 @@ const url = "https://www.hirunews.lk/local-news.php";
             });
             });
             
-const anony = `https://anony-vip-edition.herokuapp.com/api/news?apikey=hiruwa`;
-
-		const response = await got(anony);
-		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) 
-			
-			var title = json.news[0].title
-			var img = json.news[0].image
-			var rm = json.news[0].ReadMore
-			var time = json.news[0].dateAndTime
-            
-const ur = rm ;
-    axios.get(ur)
-         .then(responce => {
-         
-         const $ = cheerio.load(response.data);
-         
-         $('.main-article-section ').each((i, element) => {
-            
-        const imag = $(element).find('img').attr('data-src');
-        console.log(imag)
-        });
-        });
-        
